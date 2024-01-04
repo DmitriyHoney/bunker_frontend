bdf8321ed243cec062312a1bb98b11c33303cd82
Александр <alexandrkraft7@gmail.com>
2024-01-04 18:37:01 +0300

    add new page (GameRoom)

---
+++ src/views/GameRoom.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <section class="container">
+    <div class="cataclysm">
+      <div class="cataclysm__description">
+        <p class="base-title">Катаклизм</p>
+      </div>
+      <div class="cataclysm__conditions"></div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.cataclysm {
+  margin-top: 40px;
+  &__description {
+  }
+  &__conditions {
+  }
+}
+</style>
