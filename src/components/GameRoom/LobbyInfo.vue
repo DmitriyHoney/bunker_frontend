21ac72b4d9e59c948a32de418ce80936f9e316e4
Александр <alexandrkraft7@gmail.com>
2024-01-12 23:20:50 +0300

    fixed

---
+++ src/components/GameRoom/LobbyInfo.vue
@@ -0,0 +1,134 @@
+<script setup>
+import BaseButton from '@/components/common/BaseButton.vue';
+import BaseTooltip from '@/components/common/BaseTooltip.vue';
+
+defineProps({
+  title: String,
+  description: String,
+  img: String,
+  bunkerPlaces: Number,
+  bunkerAvailableCardsCount: Number,
+  bunkerCards: Array,
+});
+</script>
+
+<template>
+  <div class="lobby-info">
+    <div class="lobby-info__cataclysm cataclysm">
+      <div class="cataclysm__header">
+        <h3 class="base-title">{{ title }}</h3>
+        <base-button variant="primary_flat" to="game-rules">Как играть?</base-button>
+      </div>
+      <p class="cataclysm__description">{{ description }}</p>
+    </div>
+    <div class="lobby-info__bunker bunker">
+      <div class="bunker__header">
+        <div class="bunker__text">
+          <h4 class="base-title-h4">Карты бункера</h4>
+          <p class="bunker__available-cards">
+            Всего доступно {{ bunkerAvailableCardsCount }} карт бункера
+          </p>
+        </div>
+        <div class="bunker__places">Всего мест в бункере: {{ bunkerPlaces }}</div>
+      </div>
+      <ul class="bunker__cards">
+        <li class="bunker__card" v-for="(card, key) in bunkerCards" :key="key">
+          <base-tooltip :title="card.tip">1</base-tooltip>
+          {{ card.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.lobby-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+  border-radius: 42px;
+  border: 1px solid var(--regreg, #2c394b);
+  .cataclysm {
+    display: flex;
+    padding: 42px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 24px;
+    align-self: stretch;
+    border-radius: 42px 42px 0px 0px;
+    &__header {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+      align-self: stretch;
+      h3 {
+        flex: 1 0 0;
+      }
+    }
+    &__description {
+      align-self: stretch;
+      color: #fff;
+      font-family: Inter;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
+  .bunker {
+    display: flex;
+    padding: 42px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 32px;
+    align-self: stretch;
+    border-radius: 0px 0px 42px 42px;
+    &__header {
+      display: flex;
+      align-items: flex-start;
+      gap: 24px;
+      align-self: stretch;
+    }
+    &__text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 12px;
+      flex: 1 0 0;
+      h4 {
+        color: #fff;
+        font-family: Inter;
+        font-size: 32px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+        align-self: stretch;
+      }
+      p {
+        color: #fff;
+        font-family: Inter;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        align-self: stretch;
+      }
+    }
+    &__places {
+      color: #fff;
+      font-family: Inter;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      display: flex;
+      padding: 18px 32px;
+      align-items: center;
+      gap: 12px;
+      border-radius: 12px;
+      border: 1.5px solid var(--regreg, #2c394b);
+    }
+  }
+}
+</style>
---
+++ src/components/common/BasePlayers.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  players: Object,
+});
+</script>
+
+<template>
+  <div class="table-box">
+    <table>
+      <thead>
+        <tr>
+          <th>Игроки:</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(player, key) in players" :key="key">
+          <td>{{ player.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.table-box {
+  padding: 42px;
+  border-radius: 42px;
+  border: 1px solid var(--regreg, #2c394b);
+  width: 100%;
+}
+table {
+  width: 100%;
+}
+</style>
