c424eb1971e2b1894c5b07bb78b807b830b52763
Bolshakov Dmitry <vanove2013@yandex.ru>
2024-01-21 22:34:19 +0300

    fixed table

--- src/components/GameRoom/PlayersList.vue
+++ src/components/GameRoom/PlayersList.vue
@@ -1,107 +1,108 @@
 <script setup>
-import IconArrowNext from '@/components/icons/IconArrowNext.vue';
 import { onMounted, ref } from 'vue';
-
-const mainHeaders = [
-  { name: 'Игроки' },
-  { name: 'Профессия' },
-  { name: 'Чел. качества' },
-  { name: 'Чел. характеристики' },
-  { name: 'Профессия' },
-  { name: 'Профессия' },
-  { name: 'Профессия' },
-  { name: 'Профессия' },
-  { name: 'Профессия' },
-  { name: 'Профессия' },
-];
-const specHeaders = [
-  { name: 'Спец. возможность №1' },
-  { name: 'Спец. возможность №2' },
-];
-
-const newPlayersStructure = [
-  ['Евгений', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...' ],
-  ['Максим', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...' ],
-  ['Александр', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...' ],
-  ['Дмитрий', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...', 'Строитель', 'Капризный', '...' ],
-];
-
-const newPlayersStructureSpec = [
-  ['Быстрый', 'Голос' ],
-  ['Быстрый', 'Голос' ],
-  ['Быстрый', 'Голос' ],
-  ['Быстрый', 'Голос' ],
-];
+import IconArrowNext from '@/components/icons/IconArrowNext.vue';
 
 defineProps({
   players: Array,
   label: String,
 });
 
-const parent = ref(null);
+const headers = [
+  'Профессия:',
+  'Чел. качества:',
+  'Чел. характеристики:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+  'Профессия:',
+];
+
+const fixedColumn = [
+  'Евгений',
+  'Дмитрий',
+  'Александр',
+  'Максим',
+  'Новый игрок',
+];
+
+const rows = [
+  ['Строитель', 'Педант', 'test3', 'test4', 'test5', '...', 'test7', 'test1', 'test2', 'test3', 'test4', 'test5', '...', 'test7'],
+  ['Курьер', 'Изи', '...', '...', '...', 'test6', 'test7', 'test1', 'test2', 'test3', 'test4', 'test5', '...', 'test7'],
+  ['Безработный', 'Настырный', 'test3', 'test4', '...', 'test6', '...', 'test1', 'test2', 'test3', '...', 'test5', 'test6', '...'],
+  ['Презентации', 'Мигрант', '...', '...', 'test5', 'test6', 'test7', '...', 'test2', '...', 'test4', 'test5', 'test6', 'test7'],
+  ['Порноактёр', '...', 'test3', '...', 'test5', 'test6', 'test7', 'test1', 'test2', '...', 'test4', 'test5', 'test6', 'test7'],
+];
+
+const rowsAdds = [
+  ['test1', 'test2'],
+  ['test1', 'test2'],
+  ['test1', 'test2'],
+  ['test1', 'test2'],
+  ['test1', 'test2'],
+];
+
+
+const fixedBlock = ref(null);
+const widthFixedBlock = ref(0);
+
 const isScrollEnd = ref(false);
+const parent = ref(null);
 const scrollTable = () => {
-  if (isScrollEnd.value) {
-    parent.value.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-    return;
-  }
-  let scrollAmount = 0;
-  const scrollMax = parent.value.offsetWidth;
+  if (isScrollEnd.value) return parent.value.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   parent.value.scrollTo({
     top: 0,
-    left: Math.max(scrollAmount += 500, scrollMax),
+    left: Math.max(999999, parent.value.offsetWidth),
     behavior: 'smooth'
   });
 };
 
-const checkScroll = () => {
-  parent.value
-    ? isScrollEnd.value = parent.value.scrollLeft > 0
-    :  isScrollEnd.value = false;
-};
-
-const maintable = ref(null);
-const height = ref(0);
-onMounted(() => {
-  height.value = maintable.value.offsetHeight + 2;
-});
+const checkScroll = () => (parent.value ? isScrollEnd.value = parent.value.scrollLeft > 0 :  isScrollEnd.value = false);
+onMounted(() =>  widthFixedBlock.value = fixedBlock.value.offsetWidth);
 
 </script>
 
 <template>
-  <div class="players-header">
+  <div class="base-table-header">
     <h3>{{ label }}</h3>
-    <div class="players-header__slide">
+    <div class="base-table-header__slide">
       <span>{{ isScrollEnd ? 'Карточки игроков' : 'Специальные условия' }}</span>
-      <button type="button" style="transition: all ease .3s" :style="isScrollEnd ? 'transform: rotate(180deg)' : ''" @click="scrollTable"><icon-arrow-next type="single"></icon-arrow-next></button>
+      <button type="button"  @click="scrollTable"><icon-arrow-next type="single" style="transition: all ease .3s" :style="isScrollEnd ? 'transform: rotate(180deg)' : ''"></icon-arrow-next></button>
     </div>
   </div>
-<!--  -->
-  <div class="players-list hidden-scroll" ref="parent" @scroll="checkScroll">
-    <div class="players-list__content">
-      <div class="players-table players-table_main" ref="maintable">
-        <div class="players-table_main__bg" :style="`height: ${height}px; ${isScrollEnd ? 'transform: translate(-3px, -1px)' : ''}`"></div>
-        <div class="players-table_main__bg2" :style="`height: ${height}px; ${isScrollEnd ? 'transform: translate(-3px, -1px)' : ''}`"></div>
-        <div class="players-table__head">
-          <div v-for="h in mainHeaders" :key="h.name">{{ h.name }}</div>
+  <div class="base-table">
+    <div class="base-table__fixed-cols" ref="fixedBlock">
+      <div class="base-table__header">Игроки</div>
+      <div class="base-table__cell" v-for="(column, idx) in fixedColumn" :key="idx">{{ column }}</div>
+    </div>
+    <div class="base-table__content hidden-scroll" ref="parent" @scroll="checkScroll">
+      <div class="base-table__content_base">
+        <div class="base-table__headers">
+          <div class="base-table__header" v-for="(header, idx) in headers" :key="idx">
+            {{ header }}
+          </div>
         </div>
-        <div class="players-table__body">
-          <div class="players-table__row" v-for="(p, idx) in newPlayersStructure" :key="idx">
-            <div v-for="(cell, idxCell) in p" :key="idxCell">{{ cell }}</div>
+        <div class="base-table__content-row" v-for="(row, idx) in rows" :key="idx">
+          <!-- TODO fixed 'base-table__cell_center': col === '...' -->
+          <div class="base-table__content-cell base-table__cell base-table__cell_ghost" :class="{ 'base-table__cell_center': col === '...'}" v-for="(col, colIdx) in row" :key="colIdx">
+            {{ col }}
           </div>
         </div>
       </div>
-      <div class="players-table players-table_spec" id="wow">
-        <div class="players-table__head">
-          <div v-for="h in specHeaders" :key="h.name">{{ h.name }}</div>
+      <div class="base-table__content_additional">
+        <div class="base-table__headers">
+          <div class="base-table__header">Спец. возможность №1:</div>
+          <div class="base-table__header">Спец. возможность №2:</div>
         </div>
-        <div class="players-table__body">
-          <div class="players-table__row" v-for="(p, idx) in newPlayersStructureSpec" :key="idx">
-            <div v-for="(cell, idxCell) in p" :key="idxCell">{{ cell }}</div>
+        <div class="base-table__content-row" v-for="(row, idx) in rowsAdds" :key="idx">
+          <div class="base-table__content-cell base-table__cell base-table__cell_ghost_white" v-for="(col, colIdx) in row" :key="colIdx">
+            {{ col }}
           </div>
         </div>
       </div>
@@ -110,7 +111,34 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-.players {
+
+.base-table__cell {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 18px 24px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  color: #FFF;
+  background: linear-gradient(315deg, #2C394B 0%, #334756 100%);
+  min-width: 190px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  &_center {
+    justify-content: center;
+  }
+  &_ghost {
+    background: transparent;
+    border: 1px solid #2C394B;
+  }
+  &_ghost_white {
+    border: 1px solid #fff;
+  }
+}
+
+.base-table {
   &-header {
     display: flex;
     align-items: center;
@@ -150,16 +178,69 @@ onMounted(() => {
     }
   }
 }
-.players-list {
+.base-table {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 24px;
-  align-self: stretch;
-  overflow: hidden;
-  overflow-x: scroll;
-}
+  width: 100%;
+  &__headers {
+    display: flex;
+    gap: 24px;
+  }
+  &__header {
+    min-width: 190px;
+    color: #D2D2D2;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+  &__fixed-cols {
+    padding: 42px;
+    padding-right: 24px;
+    border: 1px solid red;
+    border: 1px solid rgba(204, 204, 204, 0.18);
+    border-radius: 42px 0px 0px 42px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+  &__content {
+    width: 100%;
+    display: flex;
+    gap: 24px;
+    &_base {
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      padding: 42px;
+      padding-left: 24px;
+      border: 1px solid rgba(204, 204, 204, 0.18);
+      border-radius: 0px 42px 42px 0px;
+      border-left: none;
+      width: 100%;
+      width: max-content;
+    }
+    &_additional {
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      padding: 42px;
+      padding-left: 24px;
+      border-radius: 42px;
+      width: 100%;
+      width: max-content;
+      border: 1px solid #D2D2D2;
+      background: linear-gradient(315deg, #2C394B 0%, #334756 100%);
+    }
 
+    overflow: hidden;
+    overflow-x: auto;
+    &-row {
+      display: flex;
+      gap: 24px;
+    }
+  }
+}
 .hidden-scroll {
   overflow: auto;
   -ms-overflow-style: none; /* IE 11 */
@@ -167,174 +248,6 @@ onMounted(() => {
   &::-webkit-scrollbar {
     height: 8px;
     display: none;
-  }
-}
-/* W3C standard
-   сейчас только для Firefox */
-.custom-scroll {
-  scrollbar-width: thin;
-  scrollbar-color: blue orange;
-}
-
-/* для Chrome/Edge/Safari */
-.custom-scroll::-webkit-scrollbar {
-  height: 8px;
-}
-.custom-scroll::-webkit-scrollbar-track {
-  background: #334656;
-  border-radius: 12px;
-
-}
-.custom-scroll::-webkit-scrollbar-thumb {
-  background-color: #D2D2D2;
-  border-radius: 12px;
-}
-
-.players-list.custom-scroll {
-  padding-bottom: 24px;
-}
-
-.players-list__content {
-  display: flex;
-  gap: 24px;
-}
-.players-table {
-  padding: 42px;
-  border-radius: 42px;
-  border: 1px solid  #2C394B;
-  border-left: none;
-  &_spec {
-    border: 1px solid #D2D2D2;
-    background: linear-gradient(315deg, #2C394B 0%, #334756 100%);
-    .players-table__row {
-      & div {
-        padding: 18px 24px;
-        border: 1px solid #D2D2D2;
-        border-radius: 8px;
-        color: #FFF;
-        font-family: Inter;
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 500;
-        line-height: normal;
-        background: transparent;
-        &:first-child {
-          background: transparent;
-          border: 1px solid #D2D2D2;
-        }
-      }
-    }
-  }
-  &__head {
-    display: flex;
-    justify-content: flex-start !important;
-    margin-bottom: 24px;
-    & div {
-      display: flex;
-      justify-content: flex-start !important;
-      color: #D2D2D2;
-      font-family: Inter;
-      font-size: 14px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: normal;
-    }
-  }
-  &__row {
-    & div {
-      padding: 18px 24px;
-      border: 1px solid #2C394B;
-      border-radius: 8px;
-      color: #FFF;
-      font-family: Inter;
-      font-size: 16px;
-      font-style: normal;
-      font-weight: 500;
-      line-height: normal;
-      &:first-child {
-        background: linear-gradient(315deg, #2C394B 0%, #334756 100%);
-        border: none;
-      }
-    }
-  }
-  &__row,
-  &__head {
-    display: flex;
-    gap: 24px;
-    & div {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      min-width: 170px;
-    }
-  }
-  &__body {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-  }
-}
-
-.players-table_main {
-  & .players-table_main__bg {
-    content: '';
-    display: block;
-    background-color: #000F1A;
-    width: 240px;
-    position: absolute;
-    margin: -42px;
-    border-radius: 42px 0px 0px 42px;
-    transform: translate(-3px, -1px);
-    border-radius: 42px 0px 0px 42px;
-    border: 1px solid #2C394B;
-    border-right: none;
-    z-index: 11;
-    &::after {
-      content: '';
-      position: absolute;
-      display: block;
-      height: 100%;
-      background-color: #334756;
-      height: 85%;
-      width: 1px;
-      right: 5px;
-      top: 30px;
-    }
-  }
-  & .players-table_main__bg2 {
-    content: '';
-    display: block;
-    background-color: #000F1A;
-    width: 40px;
-    position: absolute;
-    margin: -42px;
-    border-radius: 42px 0px 0px 42px;
-    transform: translate(-3px, -1px);
-    border-radius: 0px;
-    border: none;
-    border-right: none;
-  }
-  .players-table__head {
-    & div {
-      &:first-child {
-        position: absolute;
-        z-index: 12;
-      }
-      &:nth-child(2) {
-        margin-left: 220px;
-      }
-    }
-  }
-  .players-table__row {
-    & div {
-      &:first-child {
-        position: absolute;
-        z-index: 12;
-      }
-      &:nth-child(2) {
-        margin-left: 220px;
-      }
-    }
   }
 }
 </style>
